fa0fbec670435547f35ab23fa9b0996ff7ab8c2e
baptiste cellier <baptiste.cellier@outlook.com>
2025-06-16 11:37:59 +0200

    add preprocessing

--- packagename/matching.py
+++ packagename/matching.py
@@ -10,3 +10,12 @@ def get_top_20_leads(freelance_vec, prospect_df):
     similarities = cosine_similarity(freelance_vec, prospect_matrix).flatten()
     top_20_idx = similarities.argsort()[-20:][::-1]
     return prospect_df.iloc[top_20_idx].assign(similarity=similarities[top_20_idx])
+
+def get_top_20_freelances(prospect_vec, freelance_df):
+    """
+    Retourne les 20 freelances les plus proches du vecteur prospect donné.
+    """
+    prospect_matrix = vstack(freelance_df["tfidf_vector"].values)
+    similarities = cosine_similarity(prospect_vec, prospect_matrix).flatten()
+    top_20_idx = similarities.argsort()[-20:][::-1]
+    return freelance_df.iloc[top_20_idx].assign(similarity=similarities[top_20_idx])
